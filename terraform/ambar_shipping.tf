914ca2eb08712cb0b24db8abf555fe7e27515bc8
Wei Jie Leong <122815453+wleong1@users.noreply.github.com>
2024-03-18 12:56:30 +0000

    Update ambar_shipping.tf

--- terraform/ambar_shipping.tf
+++ terraform/ambar_shipping.tf
@@ -1,4 +1,3 @@
-/*
 resource "ambar_filter" "shipping_returns" {
   data_source_id  = ambar_data_source.shipping_events.resource_id
   description      = "Shipping Returns ${var.github_repository}"
@@ -14,7 +13,6 @@ resource "ambar_data_destination" "shipping_return_review" {
   username             = local.destination_username
   password             = local.destination_password
 }
-*/
 
 
 resource "ambar_data_source" "shipping_events" {
